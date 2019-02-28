3163cdcbc7d13ff452219d8a0d58a71677d22d35
Stefan Vigerske <svigerske@gams.com>
2019-02-28 18:10:28 +0100

    write out sets and parameter declarations

--- src/mosdex2gams.cpp
+++ src/mosdex2gams.cpp
@@ -3,11 +3,110 @@
 #include <cassert>
 #include <iostream>
 
+#include <set>
+#include <string>
+
 #define RAPIDJSON_HAS_STDSTRING 1
 #include "rapidjson/filereadstream.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/document.h"
 #include "rapidjson/error/en.h"
+
+using namespace rapidjson;
+
+int processInputDataModel(
+   std::ostream&  out,
+   Document&      d
+   )
+{
+   assert(d.IsObject());
+   assert(d.HasMember("INPUT_DATA_MODEL"));
+
+   auto& inputdata = d["INPUT_DATA_MODEL"];
+   assert(inputdata.IsObject());
+
+   // first collect all keynames -> they should become set declarations
+   // and all other column names
+   std::set<std::string> keynames;
+   std::set<std::string> othernames;
+   for( Value::ConstMemberIterator itr = inputdata.MemberBegin(); itr != inputdata.MemberEnd(); ++itr )
+   {
+       //std::cout << "Type of member " << itr->name.GetString() << " is " << itr->value.GetType() << std::endl;
+      assert(itr->value.IsObject());
+      for( Value::ConstMemberIterator itr2 = itr->value.MemberBegin(); itr2 != itr->value.MemberEnd(); ++itr2 )
+      {
+         // keynames are identfied by leading '*'
+         if( *itr2->name.GetString() == '*' )
+         {
+            assert(itr2->value.IsString());
+            assert(itr2->value == "String");
+
+            keynames.insert(std::string(itr2->name.GetString() + 1));
+         }
+         else
+         {
+            assert(itr2->value.IsString());
+            assert(itr2->value == "Double");
+
+            othernames.insert(itr2->name.GetString());
+         }
+      }
+   }
+
+   // declare sets
+   for( const std::string& key : keynames )
+      std::cout << "Set " << key << ";" << std::endl;
+
+   // declare further set for other column names
+   std::cout << "Set cols / ";
+   bool first = true;
+   for( const std::string& col : othernames )
+   {
+      if( !first )
+         std::cout << ", ";
+      std::cout << col;
+      first = false;
+   }
+   std::cout << " /;" << std::endl;
+
+   // declare parameters
+   for( Value::ConstMemberIterator itr = inputdata.MemberBegin(); itr != inputdata.MemberEnd(); ++itr )
+   {
+      assert(itr->value.IsObject());
+
+      std::string param(itr->name.GetString());
+      param += "(";
+
+      bool hasdata = false;
+      for( Value::ConstMemberIterator itr2 = itr->value.MemberBegin(); itr2 != itr->value.MemberEnd(); ++itr2 )
+      {
+         // keynames are identfied by leading '*'
+         if( *itr2->name.GetString() == '*' )
+         {
+            assert(itr2->value.IsString());
+            assert(itr2->value == "String");
+
+            param += std::string(itr2->name.GetString() + 1);
+            param += ", ";
+         }
+         else
+         {
+            assert(itr2->value.IsString());
+            assert(itr2->value == "Double");
+
+            hasdata = true;
+         }
+      }
+      if( hasdata )
+      {
+         param += "cols)";
+         std::cout << "Parameter " << param << std::endl;
+      }
+   }
+
+   return 0;
+}
+
 
 int main(
    int    argc,
@@ -28,16 +127,17 @@ int main(
    }
 
    char readBuffer[65536];
-   rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+   FileReadStream is(fp, readBuffer, sizeof(readBuffer));
 
-   rapidjson::Document d;
+   Document d;
    if( d.ParseStream(is).HasParseError() )
    {
-      std::cerr << "Error(offset " << d.GetErrorOffset() << "): " << rapidjson::GetParseError_En(d.GetParseError()) << std::endl;
+      std::cerr << "Error(offset " << d.GetErrorOffset() << "): " << GetParseError_En(d.GetParseError()) << std::endl;
    }
 
    fclose(fp);
 
+   processInputDataModel(std::cout, d);
 
    return EXIT_SUCCESS;
 }
