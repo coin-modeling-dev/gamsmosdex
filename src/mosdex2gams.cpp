c73198b4237e6018583c4f83c3e30af77788aeba
Stefan Vigerske <svigerske@gams.com>
2019-02-28 18:43:00 +0100

    write data as parameters or sets

--- src/mosdex2gams.cpp
+++ src/mosdex2gams.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <iostream>
 
+#include <vector>
 #include <set>
 #include <string>
 
@@ -69,7 +70,7 @@ int processInputDataModel(
    }
    std::cout << " /;" << std::endl;
 
-   // declare parameters
+   // declare parameters and dynamic sets
    for( Value::ConstMemberIterator itr = inputdata.MemberBegin(); itr != inputdata.MemberEnd(); ++itr )
    {
       assert(itr->value.IsObject());
@@ -78,6 +79,7 @@ int processInputDataModel(
       param += "(";
 
       bool hasdata = false;
+      bool first = true;
       for( Value::ConstMemberIterator itr2 = itr->value.MemberBegin(); itr2 != itr->value.MemberEnd(); ++itr2 )
       {
          // keynames are identfied by leading '*'
@@ -86,8 +88,12 @@ int processInputDataModel(
             assert(itr2->value.IsString());
             assert(itr2->value == "String");
 
+            if( !first )
+               param += ", ";
+            else
+               first = false;
+
             param += std::string(itr2->name.GetString() + 1);
-            param += ", ";
          }
          else
          {
@@ -99,12 +105,120 @@ int processInputDataModel(
       }
       if( hasdata )
       {
-         param += "cols)";
+         param += ", cols);";
          std::cout << "Parameter " << param << std::endl;
+      }
+      else
+      {
+         param += ");";
+         std::cout << "Set " << param << std::endl;
       }
    }
 
    return 0;
+}
+
+int processData(
+   std::ostream&  out,
+   Document&      d
+   )
+{
+   assert(d.IsObject());
+   assert(d.HasMember("DATA"));
+
+   auto& data = d["DATA"];
+   assert(data.IsObject());
+
+   for( Value::ConstMemberIterator itr = data.MemberBegin(); itr != data.MemberEnd(); ++itr )
+   {
+      auto& decl = d["INPUT_DATA_MODEL"][itr->name];
+
+      std::string param(itr->name.GetString());
+      param += "(";
+
+      std::vector<std::string> keys;
+      std::set<std::string> other;
+
+      bool first = true;
+      for( Value::ConstMemberIterator itr2 = decl.MemberBegin(); itr2 != decl.MemberEnd(); ++itr2 )
+      {
+         // keynames are identfied by leading '*'
+         if( *itr2->name.GetString() == '*' )
+         {
+            assert(itr2->value.IsString());
+            assert(itr2->value == "String");
+
+            if( !first )
+               param += ", ";
+            else
+               first = false;
+
+            param += std::string(itr2->name.GetString() + 1) + '<';
+
+            keys.push_back(itr2->name.GetString() + 1);
+         }
+         else
+         {
+            assert(itr2->value.IsString());
+            assert(itr2->value == "Double");
+
+            other.insert(itr2->name.GetString());
+         }
+      }
+      if( !other.empty() )
+      {
+         param += ", cols) /";
+         std::cout << "Parameter " << param << std::endl;
+      }
+      else
+      {
+         param += ") /";
+         std::cout << "Set " << param << std::endl;
+      }
+
+      assert(itr->value.IsArray());
+      for( Value::ConstValueIterator itr2 = itr->value.Begin(); itr2 != itr->value.End(); ++itr2 )
+      {
+         assert(itr2->IsObject());
+         std::string keystring;
+         bool first = true;
+         for( auto& key : keys )
+         {
+            assert(itr2->HasMember(key));
+
+            if( !first )
+               keystring += '.';
+            else
+               first = false;
+
+            keystring += '\'';
+            keystring += (*itr2)[key].GetString();
+            keystring += '\'';
+         }
+
+         if( other.empty() )
+         {
+            std::cout << "  " << keystring << std::endl;
+         }
+         else
+         {
+            for( auto& o : other )
+            {
+               if( itr2->HasMember(o) )
+               {
+                  std::cout << "  " << keystring << ".'" << o << "' " << (*itr2)[o].GetDouble() << std::endl;
+               }
+            }
+         }
+      }
+      std::cout << "/;" << std::endl;
+
+
+
+   }
+
+   return 0;
+
 }
 
 
@@ -138,6 +252,7 @@ int main(
    fclose(fp);
 
    processInputDataModel(std::cout, d);
+   processData(std::cout, d);
 
    return EXIT_SUCCESS;
 }
